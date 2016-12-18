21f326a08a0003a36c5ca03c4cb4c7157cb152f3
David Weinstein <txea@Davids-MacBook-Air.local>
2016-12-18 14:42:48 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,36 @@
+require 'pry'
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    limit(5) #self/class name isn't necessary
+  end
+
+  def self.dinghy
+    Boat.where('length < ?', 20)
+  end
+
+  def self.ship
+    Boat.where('length >= ?', 20)
+  end
+
+  def self.last_three_alphabetically
+    Boat.order('name').reverse_order.limit(3)
+  end
+
+  def self.without_a_captain
+    Boat.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    Boat.joins(:classifications).where("classifications.name = ?",'Sailboat')
+    # joins.(b_c: :class).where(class.name = sail) this works because of the expressed relationhsup
+  end
+  #   returns all boats that are sailboats (FAILED - 5)
+  def self.with_three_classifications
+    Boat.joins(:classifications).group("boats.name").having("COUNT(boats.name) =?", 3)
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,34 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    #returns all captains of catamarans
+    joins(boats: :classifications).where(classifications: {name: 'Catamaran'})
+    # (classifications.name = ?, 'Catamaran')
+  end
+
+  def self.sailors
+    #returns captains with sailboats
+    Captain.joins(boats: :classifications).where(classifications: {name: 'Sailboat'}).distinct
+  end
+
+  def self.motorboators
+    #returns captains with motorboats
+    Captain.joins(boats: :classifications).where(classifications: {name: 'Motorboat'}).distinct
+  end
+
+  def self.talented_seamen
+    # returns captains of motorboats and sailboats
+    where(id: sailors.pluck(:id) & motorboators.pluck(:id))
+  end
+
+  #Select * from captains, join on boats, join on classifications, where classifications.name = motorboats and classifications.name = sailboats
+
+  def self.non_sailors
+    # returns people who are not captains of sailboats
+    where.not(id: sailors.pluck(:id))
+  end
 end
+
+# Captain has many boats - Captain.joins(:articles)
+# category has many articles - Category.joins(:articles)
